80eace552df1d737c0ad6af1ccc5c94224dc279e
sonusasankan <sonusparsham@gmail.com>
2018-11-17 15:31:49 +0530

    Updates

---
+++ precache-manifest.e28bb9e3dd58c3bb41cd1cabe9c388ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6120730b7b641daaa6a2",
+    "url": "/no-broker/static/js/runtime~main.6120730b.js"
+  },
+  {
+    "revision": "bec9ccf0cc35fc9c4ce7",
+    "url": "/no-broker/static/js/main.bec9ccf0.chunk.js"
+  },
+  {
+    "revision": "f443d92f85c035801ddd",
+    "url": "/no-broker/static/js/1.f443d92f.chunk.js"
+  },
+  {
+    "revision": "bec9ccf0cc35fc9c4ce7",
+    "url": "/no-broker/static/css/main.a94437ce.chunk.css"
+  },
+  {
+    "revision": "f443d92f85c035801ddd",
+    "url": "/no-broker/static/css/1.904bf747.chunk.css"
+  },
+  {
+    "revision": "ceac35c338f23196e32bdce06ca541d9",
+    "url": "/no-broker/index.html"
+  }
+];
